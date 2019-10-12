06fdf86386518196ca3f76dbbd14f3b524e14f3e
Jimmy Lim <jimmy@Jimmys-Box-for-Cats.local>
2019-10-11 21:57:00 -0700

    should have seeding capabilityfor mongo database

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose');
 var Ratings = require('../seed.js');
 mongoose.connect('mongodb://localhost:27017/ratings',{ useNewUrlParser: true, useUnifiedTopology: true });
 
-
 // Set PORT# to listen on
 const PORT = 3000;
 
